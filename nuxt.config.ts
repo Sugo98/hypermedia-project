fb811a134039000428c43bd4b7d472e755e9bb34
Sugo98 <137062602+Sugo98@users.noreply.github.com>
2023-07-09 12:44:14 +0200

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    serverHandlers: [
-        {
-            route: '/server',
-            handler: '~/server/myServer/index.js',
-            middleware: true
-        }
-    ],
     ssr: true,
     runtimeConfig: {
         public:{
